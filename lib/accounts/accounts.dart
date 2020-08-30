6789e9436e1997f381067d466ff1d2212190512f
INNOCENT THOMAS <innthomas00158@gmail.com>
2020-08-30 14:44:04 +0100

    files added

--- lib/accounts/accounts.dart
+++ lib/accounts/accounts.dart
@@ -36,7 +36,42 @@ class _AccountState extends State<Account> {
       ),
       body: ListView(
         children: <Widget>[
-          MyListTile(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyListTile(),
+          ),
         ],
       ),
     );
--- lib/accounts/listTile_model.dart
+++ lib/accounts/listTile_model.dart
@@ -7,13 +7,62 @@ class MyListTile extends StatefulWidget {
 }
 
 class _MyListTileState extends State<MyListTile> {
+  _showMaterialDialog() {
+    showDialog(
+        barrierColor: Colors.yellowAccent[200],
+        context: context,
+        builder: (_) => new AlertDialog(
+              elevation: 30.0,
+              title: new Text("John Doe: 1903"),
+              content: new TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: "amount"),
+              ),
+              actions: <Widget>[
+                RaisedButton(
+                  child: Text(
+                    "withdraw",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.red[900]),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                SizedBox(
+                  width: 90,
+                ),
+                RaisedButton(
+                  elevation: 40.0,
+                  child: Text(
+                    "deposit",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green[700],
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      hoverColor: Colors.white,
+      visualDensity: VisualDensity.compact,
+      dense: false,
       contentPadding: EdgeInsets.all(8.0),
+      selectedTileColor: Colors.green[100],
       tileColor: Colors.greenAccent,
       leading: Icon(Icons.account_box),
-      trailing: Text("Bal: N23,000"),
+      trailing: Text(
+        "Bal: N23,000",
+        style: TextStyle(fontSize: 30.0),
+      ),
       title: Text(
         "John Doe",
         style: TextStyle(
@@ -30,7 +79,9 @@ class _MyListTileState extends State<MyListTile> {
           MaterialPageRoute(builder: (context) => Accountdetail()),
         );
       },
-      onTap: () {},
+      onTap: () {
+        _showMaterialDialog();
+      },
     );
   }
 }
